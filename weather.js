a2a4285efd92f872e850930e7a9326ad6c161c9c
riashaji44 <rshaji@villanova.edu>
2024-04-23 12:33:10 -0400

    update

---
+++ weather.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Button } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    container: {
+     flex: 1,
+     paddingTop: 50,
+     paddingHorizontal: 10,
+     backgroundColor: 'lightgray',
+    },
+    item: {
+      paddingVertical: 10,
+      borderWidth: 1,
+      borderColor: 'black',
+      paddingHorizontal: 10,
+    },
+  });  
